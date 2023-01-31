1ca6cf939e3142aa21c68f84b36cf7aaeb0ed3d2
jay-senjaliya <jaysenjaliya0070@gmail.com>
2023-01-31 21:42:07 +0530

    third commit code in function based component

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,59 +1,55 @@
-import React, { Component } from "react";
+import React from "react";
 
-export class NewsItem extends Component {
-  render() {
-    let { title, description, imageUrl, newsUrl, author, date, source } =
-      this.props;
-    return (
-      <div className="my-3">
-        <div className="card">
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "flex-end",
-              position: "absolute",
-              right: "0",
-            }}
+const NewsItem = (props) => {
+  let { title, description, imageUrl, newsUrl, author, date, source } = props;
+  return (
+    <div className="my-3">
+      <div className="card">
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "flex-end",
+            position: "absolute",
+            right: "0",
+          }}
+        >
+          <span
+            className="badge rounded-pill bg-danger"
+            style={{ left: "90%" }}
           >
-            <span
-              className="badge rounded-pill bg-danger"
-              style={{ left: "90%" }}
-            >
-              {source}
-            </span>
-          </div>
+            {source}
+          </span>
+        </div>
 
-          <img
-            src={
-              imageUrl
-                ? imageUrl
-                : "https://content.api.news/v3/images/bin/ff29ff039e9914d1e7eacce62fe62469"
-            }
-            className="card-img-top"
-            alt="..."
-          />
-          <div className="card-body">
-            <h5 className="card-title">{title}...</h5>
-            <p className="card-text">{description}...</p>
-            <p class="card-text">
-              <small class="text-muted">
-                By {author ? author : "Unknown"} on{" "}
-                {new Date(date).toGMTString()}
-              </small>
-            </p>
-            <a
-              href={newsUrl}
-              rel="noreferrer"
-              target="_blank"
-              className="btn btn-sm btn-dark"
-            >
-              Read More
-            </a>
-          </div>
+        <img
+          src={
+            imageUrl
+              ? imageUrl
+              : "https://content.api.news/v3/images/bin/ff29ff039e9914d1e7eacce62fe62469"
+          }
+          className="card-img-top"
+          alt="..."
+        />
+        <div className="card-body">
+          <h5 className="card-title">{title}...</h5>
+          <p className="card-text">{description}...</p>
+          <p className="card-text">
+            <small className="text-muted">
+              By {author ? author : "Unknown"} on {new Date(date).toGMTString()}
+            </small>
+          </p>
+          <a
+            href={newsUrl}
+            rel="noreferrer"
+            target="_blank"
+            className="btn btn-sm btn-dark"
+          >
+            Read More
+          </a>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default NewsItem;
