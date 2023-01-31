9e80be5931d06ee669b856c5dc0a6b7dc756820b
jay-senjaliya <jaysenjaliya0070@gmail.com>
2023-01-31 19:29:33 +0530

    first commit code in class based commponent

---
+++ src/components/NewsItem.js
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+
+export class NewsItem extends Component {
+  render() {
+    let { title, description, imageUrl, newsUrl, author, date, source } =
+      this.props;
+    return (
+      <div className="my-3">
+        <div className="card">
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "flex-end",
+              position: "absolute",
+              right: "0",
+            }}
+          >
+            <span
+              className="badge rounded-pill bg-danger"
+              style={{ left: "90%" }}
+            >
+              {source}
+            </span>
+          </div>
+
+          <img
+            src={
+              imageUrl
+                ? imageUrl
+                : "https://content.api.news/v3/images/bin/ff29ff039e9914d1e7eacce62fe62469"
+            }
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title">{title}...</h5>
+            <p className="card-text">{description}...</p>
+            <p class="card-text">
+              <small class="text-muted">
+                By {author ? author : "Unknown"} on{" "}
+                {new Date(date).toGMTString()}
+              </small>
+            </p>
+            <a
+              href={newsUrl}
+              rel="noreferrer"
+              target="_blank"
+              className="btn btn-sm btn-dark"
+            >
+              Read More
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default NewsItem;
